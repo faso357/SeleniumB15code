5d57c45c5b0bb975df5815f18d5ae534b5326de8
moazzamsadiq <moazzam@syntaxtechs.com>
2023-02-22 22:00:40 -0500

    2

---
+++ src/class02/webOrderTask.java
@@ -0,0 +1,29 @@
+package class02;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class webOrderTask {
+    public static void main(String[] args) {
+
+//        tell your project where the webdriver is located.
+//        for mac user please do not use .exe with chromedriver
+        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+//        create an instance of WebDriver
+        WebDriver driver=new ChromeDriver();
+//        open up the website
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+//        maximize the screen
+        driver.manage().window().maximize();
+
+//        send the username
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+//        send the pasword
+        driver.findElement(By.name("ctl00$MainContent$password")).sendKeys("test");
+//        click on the login button
+        driver.findElement(By.className("button")).click();
+
+
+    }
+}
